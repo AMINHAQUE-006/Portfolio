771c28b1a5720a541492da5d6b4dada5a1ca342a
Amin Haque <aminhaque5006@gmail.com>
2024-05-28 12:52:59 +0530

    some temporary changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,14 +24,16 @@ function Header() {
 
   return (
     <nav>
+      <div className="top-nav">
         <img className='nav-logo' src={logo} alt="Logo" />
+        <img className='hamburger' style={{paddingRight: "10px"}} onClick={hamburgerHandler} src={hamburger} alt="" />
+      </div>
         <ul className='nav-options'>
           <li className='nav-link'><NavLink to="/" className="nav-link-item">Home</NavLink></li>
           <li className='nav-link'><NavLink to="/About" className="nav-link-item">Aboout me</NavLink></li>
           <li className='nav-link'><NavLink to="Projects" className="nav-link-item">My Projects</NavLink></li>
           <li className='nav-link'><NavLink to="Contact" className="nav-link-item">Contact me</NavLink></li>
         </ul>
-        <img className='hamburger' style={{paddingRight: "10px"}} onClick={hamburgerHandler} src={hamburger} alt="" />
     </nav>
   )
 }
